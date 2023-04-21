3defc4d145d7833c7acc59aba83fb685798ab9e5
slash code <slashcode.dev@gmail.com>
2023-04-21 10:58:16 +0700

    session

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ server.use(bodyParser.json())
 server.use(jsonServer.defaults())
 
 const SECRET_KEY = '123456789'
-const expiresIn = '1h'
+const expiresIn = '12h'
 
 // Create a token from a payload 
 function createToken(payload) {
